2e152551da44a52d4d9abd0200297771700cf4f1
Tóth Henrik <henrik.toth@mediaworks.hu>
2024-10-28 20:37:47 +0100

    Updated to current ESPHome

--- my_components/tm1640/display.py
+++ my_components/tm1640/display.py
@@ -32,7 +32,6 @@ CONFIG_SCHEMA = disp.BASIC_DISPLAY_SCHEMA.extend(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
     await disp.register_display(var, config)
 
     clk = await cg.gpio_pin_expression(config[CONF_CLK_PIN])
